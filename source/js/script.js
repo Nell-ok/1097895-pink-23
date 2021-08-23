3ab1ca6badaafb7a989e40f3d16322a513803cd1
Nell-ok <nelli-nn@yandex.ru>
2021-08-23 17:38:55 +0300

    Автоматизация сборки личного проекта

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const pageHeader = document.querySelector('.page-header');
+
+pageHeader.classList.add('page-header--closed');
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    pageHeader.classList.remove('page-header--closed');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    pageHeader.classList.add('page-header--closed');
+  }
+});
