dff95ab563ea682a02cc2c6b0b2c3e942f44c59d
Nell-ok <nelli-nn@yandex.ru>
2021-08-28 12:23:35 +0300

    Доработка по замечаниям

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,21 @@
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-const pageHeader = document.querySelector('.page-header');
-const pageMain = document.querySelector('.page-main');
+const navMain = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".main-nav__toggle");
+const pageHeader = document.querySelector(".page-header");
+const pageMain = document.querySelector(".page-main");
 
-pageHeader.classList.add('page-header--closed');
-pageMain.classList.add('page-main--js');
-navMain.classList.remove('main-nav--nojs');
-navMain.classList.add('main-nav--closed');
+pageHeader.classList.add("page-header--closed");
+pageMain.classList.add("page-main--js");
+navMain.classList.remove("main-nav--nojs");
+navMain.classList.add("main-nav--closed");
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    pageHeader.classList.remove('page-header--closed');
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+    pageHeader.classList.remove("page-header--closed");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    pageHeader.classList.add('page-header--closed');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+    pageHeader.classList.add("page-header--closed");
   }
 });
